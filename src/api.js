ff2884a723179d09bb51bbf0e34b46b98e5b2409
haikalnuril <haikal.nuril23@gmail.com>
2025-01-03 07:57:16 +0700

    fixing again

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const customAPI = axios.create({
-    baseURL: import.meta.env.VITE_API_BASE_URL,
+    baseURL: import.meta.env.VITE_API_BASE_URL || 'https://e-commerce-mern-be.vercel.app/api/v1',
 });
 
 export default customAPI;
