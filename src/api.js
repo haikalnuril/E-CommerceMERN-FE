83e0201bbf0205cd8147d916c896fd3fccf57fe8
haikalnuril <haikal.nuril23@gmail.com>
2025-01-03 08:07:14 +0700

    fix: credentials for cookies

--- src/api.js
+++ src/api.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const customAPI = axios.create({
     baseURL: import.meta.env.VITE_API_BASE_URL || 'https://e-commerce-mern-be.vercel.app/api/v1',
+    withCredentials: true
 });
 
 export default customAPI;
